6f8463087a5c6c742681b16939a36fbccd36d5e3
David <david.portillo92@gmail.com>
2022-08-22 20:09:45 -0400

    added an unhandle promise handler

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ?? 5000;
 
 app.get("/", (req, res) => {
   res.status(200);
@@ -11,4 +11,10 @@ app.get("/", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server successfully running and listening on port ${PORT}`);
+});
+
+// handle unhandled promise rejection
+
+process.on("unhandledRejection", (reason: Error, promise) => {
+  console.log("Unhandled Rejection at: ", promise, "reason: ", reason);
 });
