2de210cb8b46f2748b3db17da05382cbf1fa5af7
David Portillo <david.portillo92@gmail.com>
2022-10-07 23:25:25 -0400

    updated error messages

--- server/middlewares/user-validator.middleware.ts
+++ server/middlewares/user-validator.middleware.ts
@@ -1,16 +1,18 @@
 import axios from "axios";
 import { NextFunction, Request, Response } from "express";
-import { check, Result, validationResult } from "express-validator";
+import { check, validationResult } from "express-validator";
 import { ValidatorError } from "../utils/customization/custom-error.util";
 import { handleErrorResponse } from "../utils/error-handling/error-response.util";
 
 const validations = [
-  check("username").isLength({ min: 3, max: 30 }),
-  check("email").isEmail(),
+  check("username")
+    .isLength({ min: 3, max: 30 })
+    .withMessage("username must be between 3 and 30 characters"),
+  check("email").isEmail().withMessage("email must be a valid email format"),
   check("password")
     .isAlphanumeric()
     .isLength({ min: 8, max: 30 })
-    .withMessage("password error"),
+    .withMessage("password must be between 8 and 30 characters"),
 ];
 
 export const validateUserBody = async function (
