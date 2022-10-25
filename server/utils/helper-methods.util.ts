e5ea3f7baf11709b3e763ea2b613178be3996f32
David Portillo <david.portillo92@gmail.com>
2022-10-24 22:10:35 -0400

    utilizing bcrypt to encrypt

--- server/utils/helper-methods.util.ts
+++ server/utils/helper-methods.util.ts
@@ -1,29 +1,10 @@
-import crypto, { CipherGCMTypes, CipherKey } from "crypto";
-import { CookieOptions } from "express";
-import { IUser } from "../models/User.model";
+import { genSaltSync, hashSync, compareSync } from "bcrypt";
 
-export const encrypt = (password: string): string => {
-  const algorithm: CipherGCMTypes = "aes-256-gcm";
-  const iv: Buffer = Buffer.alloc(16, 0);
-  const securityKey: CipherKey = crypto.randomBytes(32);
-  const cipher = crypto.createCipheriv(algorithm, securityKey, iv);
-
-  let encryptedPassword = cipher.update(password, "utf8", "hex");
-  encryptedPassword += cipher.final("hex");
-
-  return encryptedPassword;
+export const encryptInput = (input: string, saltRounds: number = 10): string => {
+  const salt: string = genSaltSync(saltRounds);
+  return hashSync(input, salt);
 };
 
-export const jwtToken = (
-  user: IUser
-): { signedToken: string; cookieOpts: CookieOptions } => {
-  const signedToken = user.getSignedJwtToken();
-
-  const options: CookieOptions = {
-    expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
-    httpOnly: true,
-    secure: process.env?.ENV === "prod" ? true : false,
-  };
-
-  return { signedToken, cookieOpts: options };
+export const matchInput = (input: string, encryptedInput: string): boolean => {
+  return compareSync(input, encryptedInput);
 };
