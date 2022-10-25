338dffbf43c1b14d61db89c047fd287283dd4fc3
David Portillo <david.portillo92@gmail.com>
2022-10-24 22:08:29 -0400

    cleaned index

--- server/index.ts
+++ server/index.ts
@@ -2,34 +2,37 @@ import express from "express";
 import connectDB from "../config/db";
 import usersRouter from "./routes/users.route";
 import adminRouter from "./routes/admin/users.admin.route";
+import { handleException } from "./utils/error-response-handling.util";
 
 const app = express();
 
-const PORT = process.env.PORT ?? 5000;
+const appInit = async () => {
+  try {
+    const PORT = process.env.PORT ?? 5000;
 
-app.get("/", (req, res) => {
-  res.status(200);
-  res.send("Welcome to Tasty Wheat API");
-});
+    app.get("/", (req, res) => {
+      res.status(200);
+      res.send("Welcome to Tasty Wheat API");
+    });
 
-connectDB(); // connect to mongoDB
+    await connectDB(); // connect to mongoDB
 
-app.use(express.json()); // body parser
+    app.use(express.json()); // body parser
 
-// mount routers
+    // mount routers
 
-// mount users router
-app.use("/api/v1/users", usersRouter);
+    // mount users router
+    app.use("/api/v1/users", usersRouter);
 
-// mount admin router
-app.use("/api/v1/admin", adminRouter);
+    // mount admin router
+    app.use("/api/v1/admin", adminRouter);
 
-app.listen(PORT, () => {
-  console.log(`Server successfully running and listening on port ${PORT}`);
-});
+    app.listen(PORT, () => {
+      console.log(`Server successfully running and listening on port ${PORT}`);
+    });
+  } catch (e) {
+    console.error(handleException(e));
+  }
+};
 
-// handle unhandled promise rejection
-
-process.on("unhandledRejection", (reason: Error, promise) => {
-  console.log("Unhandled Rejection at: ", promise, "reason: ", reason);
-});
+appInit();
